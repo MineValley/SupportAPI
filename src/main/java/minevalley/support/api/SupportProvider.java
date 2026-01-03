5d3e4abf28bf72682eeefd28bb3cc4d468d235f8
Snabeldier <vincegamezspam@gmail.com>
2026-01-04 00:30:21 +0100

    feat: add user argument and javadoc

--- src/main/java/minevalley/support/api/SupportProvider.java
+++ src/main/java/minevalley/support/api/SupportProvider.java
@@ -1,5 +1,6 @@
 package minevalley.support.api;
 
+import minevalley.core.api.users.User;
 import minevalley.support.api.ticket.Ticket;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
@@ -9,7 +10,14 @@ import javax.annotation.Nonnull;
 @ApiStatus.Internal
 public interface SupportProvider {
 
+    /**
+     * Creates a new support ticket for the given user.
+     *
+     * @param user The user creating the ticket.
+     * @return The created support ticket.
+     * @throws IllegalArgumentException If the user is null.
+     */
     @Nonnull
-    @Contract("-> new")
-    Ticket createTicket();
+    @Contract("_ -> new")
+    Ticket createTicket(@Nonnull User user) throws IllegalArgumentException;
 }
