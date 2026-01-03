10d36e285476e50c022b2d74eac7cdd4ace27b25
Snabeldier <vincegamezspam@gmail.com>
2026-01-04 00:26:03 +0100

    feat(Ticket): add getUser()

--- src/main/java/minevalley/support/api/ticket/Ticket.java
+++ src/main/java/minevalley/support/api/ticket/Ticket.java
@@ -1,4 +1,14 @@
 package minevalley.support.api.ticket;
 
+import minevalley.core.api.users.User;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
 public interface Ticket {
+
+    @Nonnull
+    @Contract(pure = true)
+    User getUser();
 }
